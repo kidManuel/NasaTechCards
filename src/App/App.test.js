bafca0c39f2b7eab5931513fe579d5adb8176fd6
kidManuel <cascallarmanuel@gmail.com>
2020-05-01 21:03:07 -0300

    Correct testing imports.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/extend-expect';
+import { test, expect } from '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
